20e7b16ce3d7ed59c283043c8fe7b11e1d888508
yuta <jp.yuta0131@gmail.com>
2024-10-13 01:06:02 +0900

    update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      spacing: {
+        15: "60px",
+      },
+      colors: {
+        menubar: "#333333",
+      },
+      boxShadow: {
+        "menubar-shadow": "0 4 10 0 #00000040",
+      },
+    },
+  },
+  plugins: [],
+};
+
+128;
